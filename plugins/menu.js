7fa25ffa9ee20b39b7518e54d9f819d337edd98e
BayuuOFC <97349632+BayuuOFC@users.noreply.github.com>
2022-01-27 18:12:36 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -25,8 +25,6 @@ const defaultMenu = {
 ┃
 ┃⬡ Uptime: *%uptime (%muptime)*
 ┃⬡ Database: %rtotalreg dari %totalreg
-┃⬡ Github:
-┃⬡ https://github.com/AlyaaXd/rfkbot
 ┃
 ┗━━━━━━⬣`.trimStart(),
   header: '┏━━〔 %category 〕━⬣',
@@ -240,10 +238,10 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
 ┃⬡ *${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length}* Chat Terbanned
 ┃⬡ *${Object.entries(global.db.data.users).filter(user => user[1].banned).length}* Pengguna Terbanned
 ┃
-┃⬡ Script by Alyaa
-┃⬡ Yt : AlyaaXzy
+┃⬡ Script by BayuuOFC
+┃⬡ Yt : BAYU OFC
 ┃⬡ Run bot : Heroku
-┃⬡ Tipe SC rfkbot
+┃⬡ Tipe SC KAGUYA-BOT
 ┗━━━━━━━━⬣`.trim(),
           "buttonText": "Klik Disini",
           "listType": "SINGLE_SELECT",
